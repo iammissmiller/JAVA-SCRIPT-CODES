8570673711be79fd0067a7abcd1347e77a6cecca
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-05-21 00:52:36 +0530

    templateliterals.js

---
+++ templateliterals.js
@@ -0,0 +1,15 @@
+const firstname = 'Praptee';
+
+const job = 'Programmer' ;
+
+const birthyear = 2006;
+
+const year = 2025;
+
+
+
+
+
+const pptnew = `I'm ${firstname} , a ${year - birthyear} year old ${job} ! `
+
+console.log(pptnew);
