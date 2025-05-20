869e67df98100dc2ba898050e4269671cd6c7ee6
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-05-21 00:55:32 +0530

    ifelse.js

---
+++ ifelse.js
@@ -0,0 +1,14 @@
+const age = 19;
+
+
+if (age >= 18) 
+{
+    console.log('PPT can start driving ');
+}
+
+else 
+{
+    const yearleft = 18 - age ;
+    
+    console.log(`PPT is too young . Wait another ${yearsleft} years .`)
+}
