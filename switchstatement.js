1928b526ed590ff143d168938dea548ea30876a4
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-05-26 23:44:28 +0530

    switchstatement.js

---
+++ switchstatement.js
@@ -0,0 +1,27 @@
+let day = "sunday";
+
+switch (day) {
+  case "monday":
+    console.log("It's Monday");
+    break;
+  case "tuesday":
+    console.log("It's Tuesday");
+    break;
+  case "wednesday":
+    console.log("It's Wednesday");
+    break;
+  case "thursday":
+    console.log("It's Thursday");
+    break;
+  case "friday":
+    console.log("It's Friday");
+    break;
+  case "saturday":
+    console.log("It's Saturday");
+    break;
+  case "sunday":
+    console.log("It's Sunday");
+    break;
+  default:
+    console.log("Not a valid day");
+}
