e01ee8f83232c8c9ba987f9dfb8113d687d005a8
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-05-30 22:28:54 +0530

    Arrowfunction.js

---
+++ Arrowfunction.js
@@ -0,0 +1,10 @@
+ // Regular function
+function add(a, b) {
+  return a + b;
+}
+
+// Arrow function
+const addArrow = (a, b) => a + b;
+
+console.log(add(3, 5));       // Output: 8
+console.log(addArrow(3, 5));  // Output: 8
