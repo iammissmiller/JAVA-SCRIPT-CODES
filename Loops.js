d752b77edb82d174d90c55f6a9162acbd40476f1
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-06-02 22:59:24 +0530

    Loops.js

---
+++ Loops.js
@@ -0,0 +1,21 @@
+// 1. For loop
+console.log("For Loop:");
+for (let i = 1; i <= 5; i++) {
+  console.log("Number: " + i);
+}
+
+// 2. While loop
+console.log("While Loop:");
+let j = 1;
+while (j <= 5) {
+  console.log("Number: " + j);
+  j++;
+}
+
+// 3. Do...while loop
+console.log("Do...While Loop:");
+let k = 1;
+do {
+  console.log("Number: " + k);
+  k++;
+} while (k <= 5);
