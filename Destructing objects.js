a6f2cfcedf6904802db09a784b66c852338bad99
Praptee Miller <139339934+iammissmiller@users.noreply.github.com>
2025-07-04 23:14:36 +0530

    Destructingobjects.js

---
+++ Destructing objects.js
@@ -0,0 +1,12 @@
+const student = {
+  name: "Praptee",
+  age: 19,
+  course: "Web Development"
+};
+
+// Destructuring the object
+const { name, age, course } = student;
+
+console.log(name);   // "Praptee"
+console.log(age);    // 19
+console.log(course); // "Web Development"
